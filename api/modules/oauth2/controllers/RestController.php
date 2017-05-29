16ed22fe7250d97100eff6808e2a691cc8f4f759
Roman Lukoyanov <ramzes.2007.90@gmail.com>
2017-05-29 11:23:56 +0300

    CORS

--- api/modules/oauth2/controllers/RestController.php
+++ api/modules/oauth2/controllers/RestController.php
@@ -8,6 +8,28 @@ use Yii;
 
 class RestController extends \filsh\yii2\oauth2server\controllers\RestController
 {
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                // restrict access to
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                // Allow only POST and PUT methods
+                'Access-Control-Allow-Headers' => ['*'],
+                // Allow only headers 'X-Wsse'
+                'Access-Control-Allow-Credentials' => true,
+                // Allow OPTIONS caching
+                'Access-Control-Max-Age' => 86400,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                'Access-Control-Expose-Headers' => [],
+            ]
+        ];
+        return $behaviors;
+    }
+    
     public function actionToken()
     {
         $response = $this->module->getServer()->handleTokenRequest();
