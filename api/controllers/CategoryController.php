123b88cde2bb55904e6f6a2fba18da6ccf144359
Roman Lukoyanov <ramzes.2007.90@gmail.com>
2017-06-01 23:03:30 +0300

    Category with content

--- api/controllers/CategoryController.php
+++ api/controllers/CategoryController.php
@@ -5,6 +5,8 @@ namespace api\controllers;
 use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
+use api\models\Category;
+use api\models\ContentCategory;
 
 class CategoryController extends BaseAuthActiveController {
     public function behaviors() {
@@ -37,4 +39,14 @@ class CategoryController extends BaseAuthActiveController {
     }
 
     public $modelClass = 'api\models\Category';
+
+    public function actions() {
+        $actions = parent::actions();
+        unset($actions['index']);
+        return $actions;
+    }
+
+    public function actionIndex(){
+        return yii\helpers\ArrayHelper::getColumn(ContentCategory::find()->groupBy(['category_id'])->select(['category_id'])->all(), 'category');
+    }
 }
